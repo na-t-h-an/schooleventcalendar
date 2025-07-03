51317cdaddfd65d853a97cb9c20974b72e522eef
freakact.js <62394230+tumulakivan@users.noreply.github.com>
2025-07-03 14:47:10 +0800

    updated some functionalities and layouts

--- FinalProject_FRONTEND/src/components/StudentDashboard/hooks/useStudentUser.jsx
+++ FinalProject_FRONTEND/src/components/StudentDashboard/hooks/useStudentUser.jsx
@@ -1,5 +1,8 @@
 export default function useStudentUser() {
-  const user = JSON.parse(localStorage.getItem('user'));
+  const user = JSON.parse(localStorage.getItem("user"));
   const username = user?.username;
-  return { user, username };
+  const firstname = user?.firstname;
+  const middlename = user?.middlename;
+  const lastname = user?.lastname;
+  return { user, username, firstname, middlename, lastname };
 }
